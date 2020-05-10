5c6f8650390700d7ef29353479c3193c1df51510
Scott Weitzner <scottweitzner@gmail.com>
2020-05-10 01:13:17 -0400

    update

--- public/component---src-pages-index-js-b0a241de49c0c617fa1c.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{RXBc:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),r=t.n(a),l=t("Bl7J"),c=t("vrFN");n.default=function(){return r.a.createElement(l.a,null,r.a.createElement("br",null),r.a.createElement(c.a,{title:"Home"}),r.a.createElement("h1",null,"Work in progress ..."))}}}]);
-//# sourceMappingURL=component---src-pages-index-js-b0a241de49c0c617fa1c.js.map
